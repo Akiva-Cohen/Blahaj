cef6538b5b453161256a5bec8b86cd217170ad36
Akiva Cohen <Akivaecohen@gmail.com>
2024-11-28 14:52:20 -0500

    added javascript

---
+++ const text = document.js
@@ -0,0 +1,9 @@
+const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+
+for (let i = 0; i < text.length; i++) {
+    if (text[i].innerHTML.includes('shark')) {
+        text[i].innerHTML = text[i].innerHTML.replace("shark", "blahaj");
+    } else if (text[i].innerHTML.includes("Shark")) {
+        text[i].innerHTML = text[i].innerHTML.replace("Shark", "Blahaj");
+    }
+}
