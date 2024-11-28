66669936eaf9e21e27b1c3f5c99951b8769ee31e
Akiva Cohen <Akivaecohen@gmail.com>
2024-11-28 16:56:54 -0500

    v1.0.0

--- Actions.js
+++ Actions.js
@@ -1,10 +1,26 @@
 console.log("working")
-const text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a')
+const types = ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'td', 'caption', 'a', 'span', 'b', 'i', 'div', 'a', 'strong', 'em', 'small', 'mark', 'del','ins', 'sub','sup',]
+for (let i = 0; i < types.length; i++) {
+    const text = document.querySelectorAll(types[i])
+    /*for (let i = 0; i < text.length; i++) {
+        if (text[i].slice(0,5) === "Shark") {
+            text[i] = "Blahaj" + text[i].substr(5);
+        }
+        if (text[i].slice(0,5) === "shark") {
+            text[i] = "blahaj" + text[i].substr(5);
+        }
+    }*/
+    for (let i = 0; i < text.length; i++) {
+        
+        if (text[i].innerHTML.includes('shark')) {
+            text[i].innerHTML = text[i].innerHTML.replace(/shark/g, "blahaj");
+        }
+        if (text[i].innerHTML.includes("Shark")) {
+            text[i].innerHTML = text[i].innerHTML.replaceAll(/Shark/g, "Blahaj");
+        }
+        if (text[i].innerHTML.includes("SHARK")) {
+            text[i].innerHTML = text[i].innerHTML.replaceAll(/SHARK/g, "BLAHAJ")
+        }
 
-for (let i = 0; i < text.length; i++) {
-    if (text[i].innerHTML.includes('shark')) {
-        text[i].innerHTML = text[i].innerHTML.replace("shark", "blahaj");
-    } else if (text[i].innerHTML.includes("Shark")) {
-        text[i].innerHTML = text[i].innerHTML.replace("Shark", "Blahaj");
     }
 }
