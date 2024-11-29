a4918fca2797d58703072a9458667a387b855d06
Akiva Cohen <Akivaecohen@gmail.com>
2024-11-29 11:48:18 -0500

    test01

--- Actions.js
+++ Actions.js
@@ -1,13 +1,35 @@
 console.log("working")
-const types = ['h1', 'h2', 'h3', 'h4', 'h5', 'p', 'li', 'td', 'caption', 'a', 'span', 'b', 'i', 'div', 'a', 'strong', 'em', 'small', 'mark', 'del','ins', 'sub','sup',]
-for (let i = 0; i < types.length; i++) {
-    const text = document.querySelectorAll(types[i])
-    for (let i = 0; i < text.length; i++) {
-        text[i].innerHTML = text[i].innerHTML.replace(/Sharks/g, "Blåhajar");
-        text[i].innerHTML = text[i].innerHTML.replace(/sharks/g,"blåhajar");
-        text[i].innerHTML = text[i].innerHTML.replace(/SHARKS/g, "BLÅHAJAR");
-        text[i].innerHTML = text[i].innerHTML.replace(/shark/g, "blåhaj");
-        text[i].innerHTML = text[i].innerHTML.replaceAll(/Shark/g, "Blåhaj");
-        text[i].innerHTML = text[i].innerHTML.replaceAll(/SHARK/g, "BLÅHAJ")
+const types = ['html'];
+let count = 0;
+const nodeList = document.querySelectorAll('*');
+let array = []
+nodeList.forEach(function (currentValue, currentIndex, listObj) {
+    if (isVisible(currentValue)) {
+        array.push(currentValue);
+    }
+});
+const text = array;
+for (let i = 0; i < text.length; i++) {
+    let chars = text[i].innerHTML.length;
+    text[i].innerHTML = text[i].innerHTML.replace(/Sharks/g, "Blåhajar");
+    text[i].innerHTML = text[i].innerHTML.replace(/sharks/g, "blåhajar");
+    text[i].innerHTML = text[i].innerHTML.replace(/SHARKS/g, "BLÅHAJAR");
+    count += ((text[i].innerHTML.length - chars) / 2);
+    chars = text[i].innerHTML.length;
+    text[i].innerHTML = text[i].innerHTML.replace(/shark/g, "blåhaj");
+    text[i].innerHTML = text[i].innerHTML.replace(/Shark/g, "Blåhaj");
+    text[i].innerHTML = text[i].innerHTML.replace(/SHARK/g, "BLÅHAJ");
+    count += (text[i].innerHTML.length - chars);   
+}
+function isVisible(element) {
+    const style = getComputedStyle(element);
+    if (style.display === 'none' || style.width <= 0 || style.height <= 0) {
+        return false;
+    } else {
+        return true;
     }
 }
+
+console.log(count);
+console.log("Sharks".length);
+console.log("Blåhajar".length);
---
+++ Storer.js
@@ -0,0 +1,8 @@
+//adds a to the amount
+function add(a) {
+
+}
+//returns current value
+function get() {
+
+}
