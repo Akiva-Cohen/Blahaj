3ccac1da5f6b191f3f9d103ce41d62536fe823de
Akiva Cohen <Akivaecohen@gmail.com>
2024-12-12 10:06:00 -0500

    color changes

--- Actions.js
+++ Actions.js
@@ -9,17 +9,22 @@ new Promise((resolve) => {
         }
     });
 });
+//a function to handle any html element
 function handleElement(element) {
+    //loops through all elements backward
     for (let i = element.childNodes.length; i-- > 0;) {
         let child = element.childNodes.item(i);
+        //checks if it is the type of element that can contain other elements
         if (child.nodeType === 1) {
             let name = child.nodeName.toLowerCase();
             if (name != 'style' && name != 'script') {
+                //recurses if it has other elements
                 handleElement(child);
-            } 
+            } // checks if it is the type of element that contains text 
         } else if (child.nodeType === 3) {
             if (typeof(child) === "string") {
             } else {
+                //fixes text
                 child.textContent = fixText(child.textContent);
             }
         }
@@ -27,18 +32,21 @@ function handleElement(element) {
 }
 function fixText(element) {
     let chars = element.length;
+    //uses regex statements to replace all  
     element = element.replace(/Sharks/g, "Blåhajar");
     element = element.replace(/sharks/g, "blåhajar");
     element = element.replace(/SHARKS/g, "BLÅHAJAR");
+    //checks the difference in text length to determine how many to add
     count += ((element.length - chars) / 2);
     chars = element.length;
     element = element.replace(/shark/g, "blåhaj");
     element = element.replace(/Shark/g, "Blåhaj");
     element = element.replace(/SHARK/g, "BLÅHAJ");
-    count += (element.length - chars);  
+    count += (element.length - chars);
     return element;
 } 
 
+//adds a number (a) to the stored number
 async function add(a) {
     if (await get() === undefined) {
         chrome.storage.local.set({"clicks" : a})
